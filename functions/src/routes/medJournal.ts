a57861be52848c64973216706a84c3ac5d2608d1
Kaela <kaelaagreen@gmail.com>
2021-11-17 20:12:21 -0500

    added post for medjournal

--- functions/src/routes/medJournal.ts
+++ functions/src/routes/medJournal.ts
@@ -23,7 +23,8 @@ routes.get("/medjournal", async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
-routes.post("/appointments", async (req, res) => {
+
+routes.post("/medjournal", async (req, res) => {
     try {
         const appointment = req.body as Appointments
         const client = await getClient();
@@ -37,6 +38,7 @@ routes.post("/appointments", async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
+
 routes.get("/mednotes", async (req, res) => {
     try {
         const client = await getClient();
@@ -52,5 +54,19 @@ routes.get("/mednotes", async (req, res) => {
     }
 });
 
+routes.post("/mednotes", async (req, res) => {
+    try {
+        const notes = req.body as Notes
+        const client = await getClient();
+        await client
+        .db()
+        .collection<Notes>("notes")
+        .insertOne(notes)
+        res.status(201).json(notes); // send JSON results
+        } catch (err) {
+        console.error("FAIL", err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 
 export default routes;
